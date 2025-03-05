b6b086abbb6995e00f371ff0e9b85341e1ed415d
Brandonttt <brandont81470@gmail.com>
2025-03-04 23:46:48 -0600

    Initial commit

---
+++ src/main/java/SistemaMedico/SistemaMedicoApplication.java
@@ -0,0 +1,13 @@
+package SistemaMedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMedicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SistemaMedico/SistemaMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package SistemaMedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
