f977ed32428c57e52dda09523f82e0a6fa7a3807
Brandonttt <brandont81470@gmail.com>
2025-04-06 20:45:16 -0600

    Update UsuarioRepository.java

--- src/main/java/SistemaMedico/repository/UsuarioRepository.java
+++ src/main/java/SistemaMedico/repository/UsuarioRepository.java
@@ -1,14 +1,21 @@
 package SistemaMedico.repository;
 
 import SistemaMedico.entity.Usuario;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 @Repository
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Usuario findByNombre(String nombre);  // EL campo es "nombre"
-    public Optional<Usuario> findByEmail(String email);
+     Optional<Usuario> findByEmail(String email);
 
+    @Query("SELECT u FROM Usuario u JOIN u.roles r WHERE r.nombre = :rolNombre")
+    List<Usuario> findByRolesNombre(@Param("rolNombre") String rolNombre);
     
+    @Query("SELECT u.nombre FROM Usuario u WHERE u.id = :id")
+String findNombreById(@Param("id") Long id);
 }
