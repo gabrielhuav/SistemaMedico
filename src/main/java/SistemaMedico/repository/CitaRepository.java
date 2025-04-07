4aa953c91f32f2fe88edb1c10ee534b525eebdbe
Brandonttt <brandont81470@gmail.com>
2025-04-06 20:45:06 -0600

    Update CitaRepository.java

--- src/main/java/SistemaMedico/repository/CitaRepository.java
+++ src/main/java/SistemaMedico/repository/CitaRepository.java
@@ -4,6 +4,14 @@ import SistemaMedico.entity.Cita;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface CitaRepository extends JpaRepository<Cita, Long> {
+
+    // Buscar citas por fecha de la próxima cita
+    List<Cita> findByFechaProximaCita(LocalDate fechaProximaCita);
+    List<Cita> findByIdPaciente(Long idPaciente);
+    List<Cita> findByIdDoctor(Long idDoctor);
 }
