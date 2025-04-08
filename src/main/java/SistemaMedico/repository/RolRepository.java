b559e113760f37b3b2ff4362d84d0a060d9b408f
gaelMarquez <gmarquezr1900@alumno.ipn.mx>
2025-04-07 22:07:16 -0600

    Update RolRepository.java

--- src/main/java/SistemaMedico/repository/RolRepository.java
+++ src/main/java/SistemaMedico/repository/RolRepository.java
@@ -1,14 +1,12 @@
 package SistemaMedico.repository;
 
 
-import SistemaMedico.entity.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import SistemaMedico.entity.Rol;
+
 public interface RolRepository extends JpaRepository<Rol, Long> {
     Optional<Rol> findByNombre(String nombre);
-   // Rol findByNombre(String nombre);  // Método para buscar un rol por su nombre
 }
